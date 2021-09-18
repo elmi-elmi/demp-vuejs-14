5a0db4060dc5723af5a836858ad45d2850202f16
elmi <shahrokhelmi@gmail.com>
2021-09-18 16:08:53 +0430

    video 4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">
+      Animate
+    </button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -10,12 +12,12 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { dialogIsVisible: false, animatedBlock: false };
   },
   methods: {
     showDialog() {
@@ -24,7 +26,10 @@ export default {
     hideDialog() {
       this.dialogIsVisible = false;
     },
-  },
+    animateBlock() {
+      this.animatedBlock = true;
+    }
+  }
 };
 </script>
 
@@ -57,6 +62,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 0.27s ease-in;
 }
 .container {
   max-width: 40rem;
@@ -68,5 +74,21 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+.animate {
+  /* transform: translateX(-100px); */
+  animation: slide-fade 0.5s ease-out forwards;
+}
+
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.27);
+  }
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
